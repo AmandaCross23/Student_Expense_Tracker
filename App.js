7fa8ab6c72d1411fadc79e93628cb1e543d5fe01
Amanda Cross <amandacross@usf.edu>
2025-11-23 15:42:37 -0500

    Added task 1 and information needed for the assignment.

---
+++ App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import { SQLiteProvider } from 'expo-sqlite';
+import ExpenseScreen from './ExpenseScreen';
+
+export default function App() {
+  return (
+    <SQLiteProvider databaseName="expenses.db">
+      <ExpenseScreen />
+    </SQLiteProvider>
+  );
+}
+
+
+
+
